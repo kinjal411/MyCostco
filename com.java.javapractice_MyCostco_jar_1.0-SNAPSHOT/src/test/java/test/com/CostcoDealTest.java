001d1dad05a51c7605dd87100f6f8a260a31b53a
Kinjal <142106905+kinjal411@users.noreply.github.com>
2023-09-29 00:32:31 -0500

    Update CostcoDealTest.java
    import added

--- com.java.javapractice_MyCostco_jar_1.0-SNAPSHOT/src/test/java/test/com/CostcoDealTest.java
+++ com.java.javapractice_MyCostco_jar_1.0-SNAPSHOT/src/test/java/test/com/CostcoDealTest.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
